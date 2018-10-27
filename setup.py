89a723a6523da9827f8adfc41d6a93656f39aabf
naari3 <naari.named@gmail.com>
2018-10-27 23:28:12 +0900

    Add setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+requires = ["numpy>=1.15.3"]
+
+
+setup(
+    name='reversi_board',
+    version='0.1',
+    description='reversi board',
+    url='https://github.com/naari3/reversi_board-py',
+    author='naari3',
+    author_email='naari.named@gmail.com',
+    license='MIT',
+    keywords='reversi othero',
+    packages=find_packages(),
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+    ],
+)
