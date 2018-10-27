c2a205df86ede0bd3a67df0c17d7b76047f6d4c8
naari3 <naari.named@gmail.com>
2018-10-28 03:29:12 +0900

    v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ requires = ["numpy>=1.15.3"]
 
 setup(
     name='reversi_board',
-    version='0.1',
+    version='0.1.1',
     description='reversi board',
     url='https://github.com/naari3/reversi_board-py',
     author='naari3',
